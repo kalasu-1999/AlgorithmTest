f5b7991eda663643a9067038108093977ec4e1df
kalasu-1999 <git@github.com:kalasu-1999/Java.git>
2020-05-22 13:20:31 +0800

    demo01

--- src/com/leetcode/Demo0002.java
+++ src/com/leetcode/Demo0002.java
@@ -15,21 +15,21 @@ package com.leetcode;
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Demo0002 {
-    public static void main(String[] args) {
-        ListNode l1 = new ListNode(2);
-        l1.next = new ListNode(4);
-        l1.next.next = new ListNode(3);
-        ListNode l2 = new ListNode(5);
-        l2.next = new ListNode(6);
-        l2.next.next = new ListNode(4);
-        ListNode listNode = addTwoNumbers(l1, l2);
-        while (listNode != null) {
-            System.out.println(listNode.val);
-            listNode = listNode.next;
-        }
-    }
+//    public static void main(String[] args) {
+//        ListNode l1 = new ListNode(2);
+//        l1.next = new ListNode(4);
+//        l1.next.next = new ListNode(3);
+//        ListNode l2 = new ListNode(5);
+//        l2.next = new ListNode(6);
+//        l2.next.next = new ListNode(4);
+//        ListNode listNode = addTwoNumbers(l1, l2);
+//        while (listNode != null) {
+//            System.out.println(listNode.val);
+//            listNode = listNode.next;
+//        }
+//    }
 
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(0), p;
         p = head;
         int flag = 0;
